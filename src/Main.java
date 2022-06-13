9645cba9ba3ea571c1262c093bd71cd4ec67769f
Дмитрий Генералов <generalovdmitriypsk@gmail.com>
2022-06-13 11:35:49 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 10_000;
+        int base = 20;
+        int bonus = price / base;
+        System.out.println("Итого бонусов: " + bonus);
+
+    }
+}
